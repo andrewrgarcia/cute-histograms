054013ecf90b651bf5279a23a780d143a6b15143
andrewrgarcia <andrewrgarcia@ufl.edu>
2021-04-01 12:00:43 -0400

    add files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cute-histograms
-A simple python program to make simple histograms from excel / csv data
+A simple python script to make simple histograms from excel / csv data
+
+Quick installation and use
+----------------------------
+1. Copy and paste the following line on the folder you want to keep cute-histograms:
+
+git clone https://github.com/andrewrgarcia/cute-histograms.git
+
+2. *(A new folder named cute-histograms will be created inside that folder)
+
+3. Create a new folder inside cute-histograms where all your spreadsheet files will be (e.g. "my_spreadsheets")]
+
+4. On a terminal, change directory to this new "cute-histograms folder". Then type:
+
+python cute-histograms.py -p "my_spreadsheets/" -f ["name of your spreadsheet file with .csv extension"]
---
+++ cute_histograms.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 11:30:26 2021
+
+@author: garci
+
+cute_histograms.py
+
+
+              |          |    _)      |                                       
+    __| |   | __|  _ \   __ \  |  __| __|  _ \   _` |  __| _` | __ `__ \   __|
+   (    |   | |    __/   | | | |\__ \ |   (   | (   | |   (   | |   |   |\__ \\
+  \___|\__,_|\__|\___|  _| |_|_|____/\__|\___/ \__, |_|  \__,_|_|  _|  _|____/
+                                               |___/                          
+
+                             by Andrew Garcia                                                                                                        
+
+"""
+print(__doc__)
+
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--file_path", default = 'my_spreadsheets/',
+                help="path where all your csv files may be. \
+                Please update the default with your common folder.")
+ap.add_argument("-f", "--filename", default = 'untitled-1.csv',
+                help = "enter the name of your file")
+
+args = vars(ap.parse_args())
+
+x = pandas.read_csv(args["file_path"]+args["filename"])
+print(x)
+
+columns = int(input('How many columns?:'))
+
+for i in range(columns):
+    plt.figure(x.columns[i]); plt.hist(x.iloc[:,i]); plt.show()
